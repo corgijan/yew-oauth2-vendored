7635cac331929cf20a8cf6db4c72ef49033d105c
Jan Vaorin <jan.vaorin@adesso.de>
2025-06-18 23:40:17 +0200

    updates

--- src/agent/mod.rs
+++ src/agent/mod.rs
@@ -277,7 +277,7 @@ where
             Msg::StartLogin(login) => {
                 if let Err(err) = self.start_login(login) {
                     // FIXME: need to report this somehow
-                    log::info!("Failed to start login: {err}");
+                    gloo::console::log!("Error starting login: {:?}", err);
                 }
             }
             Msg::Logout(logout) => self.logout_opts(logout),
